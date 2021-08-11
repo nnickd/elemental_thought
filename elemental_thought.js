427ba904fa9512e56a5ac63e98214568b66c322b
nicolas.dellitalia <nicolas.dellitalia@stsaviationgroup.com>
2021-08-11 01:24:42 -0400

    refactorifications

--- elemental_thought.js
+++ elemental_thought.js
@@ -1,13 +1,6 @@
-
-
-
-
-
-
 let points = [];
-let groups = [];
-let loopy = true;
-let toroidial = false;
+let enterToggle = true;
+let shiftToggle = false;
 let upToggle = true;
 let leftToggle = true;
 let rightToggle = true;
@@ -15,53 +8,48 @@ let downToggle = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(180)
+  background(180);
 }
 
 function draw() {
   background(180);
 
-    
-    for (let pair of pairs(points)) {
-      let p = pair[0];
-      let p2 = pair[1];
-      if (p.idx != p2.idx) {
-          Gravity(p, p2);
-          PointLine(p, p2);
-          
-          if (downToggle && !p.kill && !p2.kill) {
-            let dist = p5.Vector.dist(p.pos, p2.pos);
-            if (dist < (p.radius + p2.radius) / 4) {
-              p2.kill = true;
-            }
-          }
+  for (let pair of pairs(points)) {
+    let p = pair[0];
+    let p2 = pair[1];
+    if (p.idx != p2.idx) {
+      Gravity(p, p2);
+      PointLine(p, p2);
+
+      if (downToggle && !p.kill && !p2.kill) {
+        let dist = p5.Vector.dist(p.pos, p2.pos);
+        if (dist < (p.radius + p2.radius) / 4) {
+          p2.kill = true;
         }
+      }
     }
-    
-    if (downToggle) {
-      points = points.filter(x => !x.kill);
-    }
-    
-  // }
-  // for (let points of groups) {
-    for (let point of points) {
-      point.tick(toroidial);
-      point.draw();
-    }
-  // }
+  }
+
+  if (downToggle) {
+    points = points.filter((x) => !x.kill);
+  }
+
+  for (let point of points) {
+    point.tick(shiftToggle);
+    point.draw();
+  }
 }
 
 function keyPressed() {
   if (keyCode == ENTER) {
-    loopy = !loopy;
-    loopy ? loop() : noLoop();
+    enterToggle = !enterToggle;
+    enterToggle ? loop() : noLoop();
   }
   if (keyCode == DELETE) {
     points = [];
-    groups = [];
   }
   if (keyCode == SHIFT) {
-    toroidial = !toroidial;
+    shiftToggle = !shiftToggle;
   }
   if (keyCode == UP_ARROW) {
     upToggle = !upToggle;
@@ -83,78 +71,42 @@ function mousePressed() {
 
   let six = new PolyOps(
     (npoints = [6, 6]),
-    (radius =  [160, 80]),
-    (pradius = 10),  
+    (radius = [160, 80]),
+    (pradius = 10),
     (mass = [1000, 10]),
     (limit = 100),
     (_color = 66),
     (lineDist = 90),
     (forceDist = 90),
-    (gravity = .01)
+    (gravity = 0.01)
   );
-  
-  let _points = polyception(upToggle ? mx : 0, upToggle ? my : 0, 2, six);
-  
-  //let three = new PolyOps(
-  //  (npoints = 6),
-  //  (radius =  90),
-  //  (pradius = 10),  
-  //  (mass = 10000),
-  //  (limit = 5),
-  //  (_color = [66, 127, 166]),
-  //  (lineDist = 100),
-  //  (forceDist = 100),
-  //  (gravity = .01)
-  //);
 
-  //let _points = polyception(upToggle ? mx : 0, upToggle ? my : 0, 3, three);
-  
-  //let four = new PolyOps(
-  //  (npoints = 3),
-  //  (radius =  80),
-  //  (pradius = 30  ),  
-  //  (mass = 1000),
-  //  (limit = 300),
-  //  (_color = 66),
-  //  (lineDist = 90),
-  //  (forceDist = 90),
-  //  (gravity = .01)
-  //);
+  let _points = polyception(upToggle ? mx : 0, upToggle ? my : 0, 1, six);
 
-  //let _points = polyception(upToggle ? mx : 0, upToggle ? my : 0,9, four);
-  
-  
-  
-  
-  for (let i = 0; i < _points.length; i++) {
-      for (let j = i; j < _points.length; j++) {
-        if (i != j) {
-          let p = _points[i];
-          let p2 = _points[j];
-          if (!p.kill && !p2.kill) {
-            
-            let dist = p5.Vector.dist(p.pos, p2.pos);
-            if (dist < (p.radius + p2.radius) / 4) {
-              p2.kill = true;
-            }
+  for (let pair of pairs(_points)) {
+    let p = pair[0];
+    let p2 = pair[1];
+    if (p.idx != p2.idx) {
+      if (!p.kill && !p2.kill) {
+        let dist = p5.Vector.dist(p.pos, p2.pos);
+        if (dist < (p.radius + p2.radius) / 4) {
+          p2.kill = true;
         }
-
       }
     }
   }
-  
+
   points = points.concat(_points);
   console.log(points.length);
-  
-  points = points.filter(x => !x.kill);
+
+  points = points.filter((x) => !x.kill);
   console.log(points.length);
-  
-  if (!loopy) {
+
+  if (!enterToggle) {
     for (let point of points) {
       point.draw();
     }
   }
-  // groups.push(_points);
 }
 
 // function mouseDragged() {
